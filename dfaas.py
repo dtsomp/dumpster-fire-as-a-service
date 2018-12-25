521d4c5a4168a6d10e0e530a463ff9f26129e1ac
Dimitris Tsompanidis <dtsomp@gmail.com>
2018-12-25 14:12:35 +0100

    Working prototype

---
+++ dfaas.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import random
+
+app = Flask(__name__)
+
+api = Api(app)
+
+dumpsterfires = [
+    { 
+        'desc': 'container',
+        'url': 'https://media.giphy.com/media/26FPy3QZQqGtDcrja/giphy.gif'
+    },
+    { 
+        'desc': 'under control',
+        'url': 'https://media.giphy.com/media/ZNBXrNJfS3bQk/giphy.gif'
+    },
+    {
+        'desc': 'selfie',
+        'url': 'https://media.giphy.com/media/SyqAptNmn3fyM/giphy-downsized-large.gif'
+    },
+    {
+        'desc': 'floating',
+        'url': 'https://media.giphy.com/media/QLyhWVTvAHbAbAdWcp/giphy.gif'
+    }
+]
+
+class Random(Resource):
+    def get(self):
+        if len(dumpsterfires) > 0:    
+            return random.choice(dumpsterfires)
+        return { 'dumpster fire': None }, 404
+
+        
+
+api.add_resource(Random, '/random')
+app.run(port=5000)
